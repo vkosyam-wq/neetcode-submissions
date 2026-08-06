4ab02d8d07afa58701eee936c8a37bc07ce6af13
vkosyam-WD <vkosyam@gmail.com>
2026-08-06 15:46:31 -0700

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            sb.append("<>@");
+            if (s.isEmpty()) {
+                sb.append("!");   // marker for empty string
+            } else {
+                sb.append(s);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        String[] parts = str.split("<>@");
+        List<String> result = new ArrayList<>();
+
+        for (int i = 1; i < parts.length; i++) {
+            if (parts[i].equals("!")) {
+                result.add("");      // empty string
+            } else {
+                result.add(parts[i]);
+            }
+        }
+
+        return result;
+    }
+}
