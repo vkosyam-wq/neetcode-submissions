f0c9605c2005522f06a7a945126bc60efd883796
vkosyam-WD <vkosyam@gmail.com>
2026-08-14 12:23:29 -0700

    Add: minimum-stack - submission-9

---
+++ Data Structures & Algorithms/minimum-stack/submission-9.java
@@ -0,0 +1,42 @@
+class MinStack {
+
+    private Stack<Integer> minS;
+    Stack<Integer> newStack = new Stack<>();
+
+    public MinStack() {
+        this.minS = new Stack<>();
+    }
+    
+    public void push(int val) {
+        minS.push(val);
+        if(newStack.empty() || minS.peek() <= newStack.peek()) newStack.push(minS.peek());
+    
+    }
+    
+    public void pop() {
+        if(!newStack.empty() && minS.peek() == newStack.peek())
+        {
+            newStack.pop();
+            minS.pop();
+        }
+        else{
+            minS.pop();
+        }
+        
+
+
+    }
+    
+    public int top() {
+        return minS.peek();
+    }
+    
+    public int getMin() {
+        if(!newStack.empty())
+        {
+            return newStack.peek();
+
+        }
+        return -1;
+    }
+}
