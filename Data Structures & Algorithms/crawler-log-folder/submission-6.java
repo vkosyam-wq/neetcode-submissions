de22c25795e1c988eb735efe47eb6f007a45464f
vkosyam-WD <vkosyam@gmail.com>
2026-08-14 13:15:52 -0700

    Add: crawler-log-folder - submission-6

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-6.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minOperations(String[] logs) {
+       
+        int minOp = 0;
+        Stack<String> logsStack = new Stack<>();
+        for(String folderOp : logs)
+        {
+            logsStack.push(folderOp);
+            if(logsStack.peek().equals("../") && minOp == 0) continue;
+
+            if(logsStack.peek().equals("./")) continue;
+
+            if(logsStack.peek().equals("../"))
+            {
+                minOp--;
+                logsStack.pop();
+            }
+            
+            else
+            {
+                minOp++;
+                logsStack.pop();
+            }
+
+        }
+        return minOp;
+        
+    }
+}
