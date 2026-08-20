6a1c3c4573a8f4ada2e92f641077090ae9621573
vkosyam-WD <vkosyam@gmail.com>
2026-08-20 11:07:41 -0700

    Add: asteroid-collision - submission-3

---
+++ Data Structures & Algorithms/asteroid-collision/submission-3.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> asteroidStack = new Stack<>();
+
+
+        for(int asteroid : asteroids)
+        {
+            if(!asteroidStack.empty() && asteroid < 0)
+            {
+                while(!asteroidStack.empty() && asteroidStack.peek() > 0 && Math.abs(asteroid) > asteroidStack.peek()) asteroidStack.pop();
+
+                if(!asteroidStack.empty() && !asteroidStack.empty() && asteroidStack.peek() > 0 && Math.abs(asteroid) == asteroidStack.peek())
+                {
+                    asteroidStack.pop();
+                    continue;
+                }
+                if(!asteroidStack.empty() && asteroidStack.peek() > 0 && Math.abs(asteroid) < asteroidStack.peek()) continue;
+
+                
+            }
+
+            asteroidStack.push(asteroid);
+
+            
+
+
+            
+        }
+
+
+        int n = asteroidStack.size();
+
+        List<Integer> asteroidList = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) asteroidList.add(0,asteroidStack.pop());
+
+        int[] asteroidResult = new int[n];
+        for(int i = 0; i < n; i++) asteroidResult[i] = asteroidList.get(i);
+        return asteroidResult;
+
+
+
+
+
+    }
+    
+    
+}
